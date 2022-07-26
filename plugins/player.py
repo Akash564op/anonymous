6247f8db90ab13a27c3cdcbcf60c174495cf8ca0
Kuhmarvivek <109455833+Kuhmarvivek@users.noreply.github.com>
2022-07-26 21:57:33 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -75,7 +75,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
                 await f.close()
 
     image1 = Image.open("./background.png")
-    image2 = Image.open("https://i.postimg.cc/28jDP97b/1084517-200.png")
+    image2 = Image.open("resource/wallperr.jpg")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
